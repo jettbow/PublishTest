5307b182a63cbdecc1882e95f9a3a43993ba1429
Jett Hsieh <jettbow@gmail.com>
2017-09-25 16:20:26 +0800

    test

---
+++ merge-release-branch.sh
@@ -0,0 +1,22 @@
+set -e
+if [ "$1" == "" ]
+then
+  echo "Error:Please specify the name of release branch"
+  exit
+fi 
+if [ "$2" == "" ]
+then
+  echo "Error:Please specify the name of release Tag"
+  exit
+fi 
+if [[ "$2" == v* ]]
+then
+  echo "Error:Tag name should not start with 'v'"
+  exit
+fi 
+
+git fetch origin master dev "$1"
+git checkout -B master origin/master
+git checkout -B dev origin/dev
+git checkout -B "$1" origin/"$1"
+git branch
