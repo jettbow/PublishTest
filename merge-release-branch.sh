b2b3668f68013e222312c362181472492b7d17fd
Jett Hsieh <jettbow@gmail.com>
2017-09-25 18:39:15 +0800

    Update scripts

--- merge-release-branch.sh
+++ merge-release-branch.sh
@@ -8,19 +8,14 @@ echo "Please enter the name of the release branch : (e.g release/2.33.0)"
 read release_branch
 echo "Fetching $release_branch from remote ..."
 git fetch origin "$release_branch"
-echo "Please enter the name of new Tag : (e.g 2.33.0)"
+echo "Please enter the name of new Tag : (e.g v2.33.0)"
 read release_tag
-while [[ "$release_tag" == v* ]]
-do
-  echo "Error: Tag name should not start with 'v'"
-  echo "Please enter a new Tag name : (e.g 2.33.0)"
-  read release_tag
-done 
 
 git fetch origin master dev "$release_branch"
 git checkout -B master origin/master
 git checkout -B dev origin/dev
 git checkout -B "$release_branch" origin/$release_branch
+
 git checkout dev 
 echo "Checking conflicts between dev and $release_branch"
 if [[ `git merge $release_branch --no-commit --no-ff | grep "CONFLICT"` ]]; then
@@ -30,6 +25,7 @@ if [[ `git merge $release_branch --no-commit --no-ff | grep "CONFLICT"` ]]; then
 	exit
 fi
 git merge --abort
+
 git checkout master
 echo "Checking conflicts between master and $release_branch"
 if [[ `git merge $release_branch --no-commit --no-ff | grep "CONFLICT"` ]]; then
@@ -39,12 +35,18 @@ if [[ `git merge $release_branch --no-commit --no-ff | grep "CONFLICT"` ]]; then
 	exit
 fi
 git merge --abort
-git merge "$release_branch"
+
+git checkout dev 
+git merge --no-ff "$release_branch"
 git push origin dev
+
 git checkout master 
-git merge "$release_branch"
+git merge --no-ff "$release_branch"
 git push origin master
+
 git tag -a "$release_tag" -m '"$release_tag"'
 git push origin "$release_tag"
 read -p "Press enter to continue opening Carousell-Android page for adding new Release in GitHub..."
 open https://github.com/carousell/Carousell-Android/releases/new?tag="$release_tag"
+
+git checkout "$current_branch"
