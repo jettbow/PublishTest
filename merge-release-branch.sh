5cbefcab8b7fe1ff03fb01bd5e7e0ec1eefbf9cf
Jett Hsieh <jettbow@gmail.com>
2017-09-25 18:12:31 +0800

    Update script

--- merge-release-branch.sh
+++ merge-release-branch.sh
@@ -5,7 +5,7 @@ if [[ `git status --porcelain` ]]; then
 fi
 echo "Please enter the name of the release branch (e.g release/2.33.0): "
 read release_branch
-echo "fetch origin $release_branch..."
+echo "Fetching origin $release_branch..."
 git fetch origin "$release_branch"
 echo "Please enter the name of new Tag (e.g 2.33.0): "
 read release_tag
@@ -23,8 +23,23 @@ read -p "Press enter to continue"
 git fetch origin master dev "$release_branch"
 git checkout -B master origin/master
 git checkout -B dev origin/dev
-git checkout -B "$release_branch" origin/"$release_branch"
+git checkout -B "$release_branch" origin/$release_branch
 git checkout dev 
+echo "Checking conflicts between dev and $release_branch"
+if [[ `git merge $release_branch --no-commit --no-ff | grep "CONFLICT"` ]]; then
+	git merge --abort
+	echo "There are conflicts between dev and $release_branch"
+	exit
+fi
+git merge --abort
+git checkout master
+echo "Checking conflicts between master and $release_branch"
+if [[ `git merge $release_branch --no-commit --no-ff | grep "CONFLICT"` ]]; then
+	git merge --abort
+	echo "There are conflicts between dev and $release_branch"
+	exit
+fi
+git merge --abort
 git merge "$release_branch"
 git push origin dev
 git checkout master 
