25b97d2a7248f5efcd6bb9a6967ae781917e0ae7
Jett Hsieh <jettbow@gmail.com>
2017-09-28 15:11:51 +0800

    temp

---
+++ source/bump-major-version.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+root_dir=$(dirname $0)/..
+text=""
+env_version_name=""
+env_version_code=0
+newline=$'\n'
+file="$root_dir/gradle.properties"
+
+if [ -f "$file" ]
+then
+  while IFS='$newline' read line
+  do
+    #echo $line
+    if [[ ($line == \#*) || ($line == \org.gradle.jvmargs*)]]
+    then
+      text=$text$line$newline
+    else
+      #echo $line
+      IFS='=' 
+      set -- $line
+      key=$1
+      value=$2
+      #echo $key
+      if [ "$key" != "" ]
+      then
+        eval "${key}='${value}'"
+        if [ "$key" == "envVersionName" ]
+        then
+          major=$(echo $value | cut -d'.' -f1)
+          minor=$(echo $value | cut -d'.' -f2)
+          env_version_name=$major.$((${minor}+1)).0
+          text=$text"$key=${env_version_name}"$newline
+        elif [ "$key" == "envVersionCode" ]   
+        then
+          env_version_code=$((${value}+1))
+          text=$text"$key=$((${value}+1))"$newline
+        else
+          text=$text"$key=$value"$newline
+        fi
+      fi
+    fi
+  done < "$file"
+fi
+echo "${text}" > ./gradle.properties
+echo "Bump version $env_version_name($env_version_code)"
+now="$(date +'%Y-%m-%d')"
+sed -i '' "1 s/$/ - $now/" $root_dir/CHANGELOG.md
+echo -e "## [$env_version_name]\n### Added\n### Changed\n### Removed\n### Fixed\n$(cat $root_dir/CHANGELOG.md)" > $root_dir/CHANGELOG.md
+git add -A
+git commit -m "Bump version $env_version_name($env_version_code)"
---
+++ source/create-tag.sh
@@ -0,0 +1,47 @@
+set -e
+if [ "$1" == "" ]
+then
+  echo "Error:Please specify the build number"
+  exit
+fi 
+root_dir=$(dirname $0)/..
+tag=""
+maror=""
+minor=""
+build=$1
+newline=$'\n'
+file="$root_dir/gradle.properties"
+
+if [ -f "$file" ]
+then
+  while IFS='$newline' read line
+  do
+    if [[ ($line == \#*) || ($line == \org.gradle.jvmargs*)]]
+    then
+      text=$text$line$newline
+    else
+      IFS='=' 
+      set -- $line
+      key=$1
+      value=$2
+      if [ "$key" != "" ]
+      then
+        eval "${key}='${value}'"
+        if [ "$key" == "envVersionName" ]
+        then
+          major=$(echo $value | cut -d'.' -f1)
+          minor=$(echo $value | cut -d'.' -f2)
+          tag=$major"."$minor"."$build
+          text=$text"$key=${tag}"$newline
+        else
+          text=$text"$key=$value"$newline
+        fi
+      fi
+    fi
+  done < "$file"
+fi
+echo "${text}" > $root_dir/gradle.properties
+cd $root_dir/
+git tag -a $tag -m "$tag"
+git push origin $tag
+
