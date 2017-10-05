29cb5ea1fa4410170eb7ea74763c20ccefb8b054
Jett Hsieh <jettbow@gmail.com>
2017-10-05 12:35:47 +0800

    Update scripts

--- scripts/bump-dev.sh
+++ scripts/bump-dev.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
-if [ "$1" == "" ]
-then
-  echo "Error:Please specify the version name"
-  exit
-fi
 root_dir=$(dirname $0)/..
-env_version_name="$1"
 cd $root_dir/
 git checkout dev
 $root_dir/scripts/bump-minor-version.sh
